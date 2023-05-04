62d8c56a0fdefd080fde0146a1e7a95dd187a55f
fendishatech <liyukibir@gmail.com>
2023-05-04 13:15:46 -0700

    seed table

--- api/server.js
+++ api/server.js
@@ -1,7 +1,13 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
+const migrate_table = require("./src/helpers/migrate_table");
+const seed_table = require("./src/helpers/seed_table");
 
 const app = express();
+
+migrate_table();
+
+seed_table();
 
 app.listen(process.env.PORT, () => {
   console.log(`App running on http://localhost:${process.env.PORT}`);
--- api/src/helpers/seed_table.js
+++ api/src/helpers/seed_table.js
@@ -1,19 +1,32 @@
+const User = require("../models/user.model");
+
 const seed_table = async () => {
-  const user = {
-    username: "kidus",
-    passowrd: 123456,
-    user_role: "admin",
-  };
-  const user2 = {
-    username: "kidus2",
-    passowrd: 123456,
-    user_role: "user",
-  };
+  const user = [
+    {
+      username: "kidus",
+      password: 123456,
+      user_role: "admin",
+    },
+    {
+      username: "kidus2",
+      password: 123456,
+      user_role: "user",
+    },
+  ];
   try {
-    await User.create(user);
-    await User.create(user1);
+    // user.map(async (u) => {
+    //   await User.create(u);
+    // });
+
+    const users = await User.findAll();
+
+    users.forEach((user) => {
+      console.log(
+        `Id : ${user.id} \nUsername : ${user.username}, \n Password : ${user.password} \n Role ${user.user_role}`
+      );
+    });
   } catch (error) {
-    console.log(`Error Seeding table : ${e.message}`);
+    console.log(`Error Seeding table : ${error.message}`);
   }
 };
 
